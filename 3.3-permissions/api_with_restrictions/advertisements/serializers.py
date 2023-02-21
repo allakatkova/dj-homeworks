3155f40a888deea520393b58fbddb656ad4de8d6
Alla Katkova <allakatkova@bk.ru>
2023-02-21 22:11:33 +0500

    Tasks complited

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,20 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from advertisements.models import Advertisement
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer для пользователя."""
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name',
-                  'last_name',)
+        fields = ('id', 'username', 'first_name', 'last_name',)
 
 
 class AdvertisementSerializer(serializers.ModelSerializer):
-    """Serializer для объявления."""
 
     creator = UserSerializer(
         read_only=True,
@@ -22,24 +20,35 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Advertisement
-        fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at', )
+        fields = ('id', 'title', 'description',
+                  'creator', 'status', 'created_at', )
 
     def create(self, validated_data):
-        """Метод для создания"""
 
         # Простановка значения поля создатель по-умолчанию.
         # Текущий пользователь является создателем объявления
         # изменить или переопределить его через API нельзя.
-        # обратите внимание на `context` – он выставляется автоматически
+        # обратите внимание на 'context' – он выставляется автоматически
         # через методы ViewSet.
-        # само поле при этом объявляется как `read_only=True`
+        # само поле при этом объявляется как 'read_only=True'
+
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
     def validate(self, data):
-        """Метод для валидации. Вызывается при создании и обновлении."""
 
-        # TODO: добавьте требуемую валидацию
+        request = self.context['request'].method
+        user = self.context['request'].user
+
+        # Проверяем что пользователь имеет менее 10 объявлений
+        # self.instance отсутствует если объект создается.
+        # Это условие нужно чтобы проходила валидация при редактировании
+
+        if request == 'POST' and Advertisement.objects.filter(creator=user, status='OPEN').count() >= 10:
+            raise ValidationError('You cannot create more than 10 open ads')
+
+        if request in ('PATCH', 'PUT') and data.get('status') == 'OPEN' \
+                and Advertisement.objects.filter(creator=user, status='OPEN').count() >= 10:
+            raise ValidationError('You cannot create more than 10 open ads')
 
         return data
